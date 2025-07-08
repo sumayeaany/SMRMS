caff12d31b7c9aa448a310d9ab6f4ecefbbf1e61
Sabah Alam Tahaa <satahaa12345@gmail.com>
2025-07-09 04:35:15 +0600

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/SMRMS
-# Build directory: E:/SMRMS/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestPatient "E:/SMRMS/cmake-build-debug/test_patient.exe")
-set_tests_properties(TestPatient PROPERTIES  _BACKTRACE_TRIPLES "E:/SMRMS/CMakeLists.txt;36;add_test;E:/SMRMS/CMakeLists.txt;0;")
